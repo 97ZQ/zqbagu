2552f429092f93a952544f2db423778a36ecec0b
zq <2397873741@qq.com>
2024-10-21 15:50:02 +0800

    初始化项目

---
+++ src/test/java/com/zq/zqbagu/manager/CosManagerTest.java
@@ -0,0 +1,21 @@
+package com.zq.zqbagu.manager;
+
+import javax.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Cos 操作测试
+ *
+ */
+@SpringBootTest
+class CosManagerTest {
+
+    @Resource
+    private CosManager cosManager;
+
+    @Test
+    void putObject() {
+        cosManager.putObject("test", "test.json");
+    }
+}
